2698c6f9d824c555ff3f4c71e7be9825ad129cf2
Akanksha161 <76396493+Akanksha161@users.noreply.github.com>
2022-05-12 20:54:01 +0530

    initial commit

---
+++ Day6/Nutbolt.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Nutbolt
+{
+ public static void main(String[] args)
+ {
+       int bolts;
+       int nuts;
+       int washer;
+       
+     Scanner ab=new Scanner(System.in);
+     System.out.println("Enter number of Bolts");
+     bolts=ab.nextInt();
+     System.out.println("Enter number of nuts");
+     nuts=ab.nextInt();
+     System.out.println("Enter number of washer");
+     bolts=ab.nextInt();
+     
+  }   
+}     
